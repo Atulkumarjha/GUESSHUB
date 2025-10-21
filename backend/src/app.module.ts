ce556019a72fe7faaf2548826ba3d3a4f3e1fcdc
Atul kumar jha <jhaak301@gmail.com>
2025-10-21 18:56:25 +0530

    Created the entry point resources for all resources and backend entry point

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MarketsModule } from './markets/markets.module';
+import { BetsModule } from './bets/bets.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MarketsModule, BetsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
