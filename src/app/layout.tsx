dac2fd71f1a89d6b99ee8e3b4d5d2aba83da753f
Atul kumar jha <jhaak301@gmail.com>
2025-10-25 18:07:07 +0530

    Done with setup for next auth with google provider and also solve the issue relate to network access for mongoDB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { DottedGlowBackground } from "../../components/ui/dotted-glow-background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,21 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white.`}
     <html lang="en" className="dark bg-black">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative bg-black text-white`}
       >
-        <DottedGlowBackground 
-          className="fixed inset-0 z-0"
-          darkColor="rgba(255,255,255,0.5)"
-          darkGlowColor="rgba(59, 130, 246, 0.7)"
-        />
-        <div className="relative z-10">
-          {children}
-        </div>
+        <div className="relative z-10">{children}</div>
       </body>
     </html>
   );
