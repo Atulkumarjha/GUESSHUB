db0a8595363a1f31366c4d02f0da4026f10b3e4a
Atul kumar jha <145698735+Atulkumarjha@users.noreply.github.com>
2025-10-30 11:18:33 +0530

    Categories (#9)
    * WIP: working on user login and flow of authentication using google credentials
    
    * Succesfully integrated the login and sign up part for user using google provider and next-auth 2.0
    
    * Completed the market creation and showing on frontend
    
    * Create backend for market creation and showing on frontend with betting system
    
    * Category Schema . API endpoint for Categories . UI/UX to add and display categories

--- lib/models/Category.ts
+++ lib/models/Category.ts
@@ -1,6 +1,21 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface ICategory extends Document {
+  name: string;
+  slug: string;
+  description: string;
+}
+
+const CategorySchema = new Schema<ICategory>(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Category ||
+  mongoose.model<ICategory>("Category", CategorySchema);
     name: string;
     slug: string;
     description: string;
